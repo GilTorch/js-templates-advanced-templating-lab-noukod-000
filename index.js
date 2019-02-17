a7c0e7317fc3ad91ce51844cec68ed01aaa40990
Gilbert Torchon <gilbert@noukod.com>
2019-02-17 01:45:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  Handlebars.registerHelper("displayIngredient",function(){
+  return new Handlebars.SafeString("<li name='ingredients'>"+this.name+"</li>")
+})
+Handlebars.registerPartial("recipeDetailsPartial",document.getElementById("recipe-details-partial").innerHTML);
+
+Handlebars.registerPartial("recipeFormPartial",document.getElementById("recipe-form-partials").innerHTML);
+
+renderTemplate(document.getElementById("recipe-form-template").innerHTML,document.getElementById("main"),recipe);
+
 }
+
+function renderTemplate(template,root,content){
+  root.innerHTML=Handlebars.compile(template)(content)
+}
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
